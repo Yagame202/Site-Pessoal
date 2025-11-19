95a5aa89ddcd19b0a04aabc38ad10e6a3ff606fd
Yagame <Gustavo_guaresi@estudante.sesisenai.org.br>
2025-11-19 18:39:28 -0300

    Add files via upload

--- valorant-site/js/armas.js
+++ valorant-site/js/armas.js
@@ -1,1 +1,61 @@
-alert("Essa e parte das armas do Valorant!");
+alert("Essa e a parte que mostra o nosso Arsenal!");
+
+const imagem1 = document.getElementById('img1');
+
+imagem1.addEventListener('mouseenter', () => {
+  imagem1.style.transform = 'scale(1.2)';
+});
+
+imagem1.addEventListener('mouseleave', () => {
+  imagem1.style.transform = 'scale(1)';
+});
+
+const imagem2 = document.getElementById('img2');
+
+imagem2.addEventListener('mouseenter', () => {
+  imagem2.style.transform = 'scale(1.2)';
+});
+
+imagem2.addEventListener('mouseleave', () => {
+  imagem2.style.transform = 'scale(1)';
+});
+
+const imagem3 = document.getElementById('img3');
+
+imagem3.addEventListener('mouseenter', () => {
+  imagem3.style.transform = 'scale(1.2)';
+});
+
+imagem3.addEventListener('mouseleave', () => {
+  imagem3.style.transform = 'scale(1)';
+});
+
+const imagem4 = document.getElementById('img4');
+
+imagem4.addEventListener('mouseenter', () => {
+  imagem4.style.transform = 'scale(1.2)';
+});
+
+imagem4.addEventListener('mouseleave', () => {
+  imagem4.style.transform = 'scale(1)';
+});
+
+const imagem5 = document.getElementById('img5');
+
+imagem5.addEventListener('mouseenter', () => {
+  imagem5.style.transform = 'scale(1.2)';
+});
+
+imagem5.addEventListener('mouseleave', () => {
+  imagem5.style.transform = 'scale(1)';
+});
+
+const imagem6 = document.getElementById('img6');
+
+imagem6.addEventListener('mouseenter', () => {
+  imagem6.style.transform = 'scale(1.2)';
+});
+
+imagem6.addEventListener('mouseleave', () => {
+  imagem6.style.transform = 'scale(1)';
+});
